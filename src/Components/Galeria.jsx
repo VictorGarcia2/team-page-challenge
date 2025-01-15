bcd514a091d4c21a932cc2ffaaa520a99d9932a7
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-01-15 00:14:52 -0600

    this is

--- src/Components/Galeria.jsx
+++ src/Components/Galeria.jsx
@@ -5,41 +5,41 @@ function Galeria() {
     <div className="Galeria">
       <div className="GaleriaIzq">
         <Imagen
-          imagen="/public/photos/photo1.png"
+          imagen="/photos/photo1.png"
           nombre={"Bill Mahoney"}
           atributo={"Dueño del producto"}
         />
         <div className="abajo">
         <Imagen
           
-          imagen="/public/photos/photo3.png"
+          imagen="/photos/photo3.png"
           nombre={"Shae Le"}
           atributo={"Líder tecnológico"}
         />
         </div>
 
         <Imagen
-          imagen="/public/photos/photo5.png"
+          imagen="/photos/photo5.png"
           nombre={"Griff Richards"}
           atributo={"Desarrollador"}
         />
       </div>
       <div className="GaleriaDer">
         <Imagen
-          imagen="/public/photos/photo2.png"
+          imagen="/photos/photo2.png"
           nombre={"Saba Cabrera"}
           atributo={"Directora de arte"}
         />
         <div className="abajo">
         <Imagen
           className="abajo"
-          imagen="/public/photos/photo4.png"
+          imagen="/photos/photo4.png"
           nombre={"Skylah Lu"}
           atributo={"Diseñadora UX"}
         />
         </div>
         <Imagen
-          imagen="/public/photos/photo6.png"
+          imagen="/photos/photo6.png"
           nombre={"Stan John"}
           atributo={"Desarrollador"}
         />
